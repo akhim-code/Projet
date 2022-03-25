e0d1b275d6bd759b2b7db7d7680aa3137719903d
akhim <albertkhim928@gmail.com>
2022-03-25 12:51:13 +0100

    20

--- src/main/java/net/macompagnieakhim/Projet/beans/Animal.java
+++ src/main/java/net/macompagnieakhim/Projet/beans/Animal.java
@@ -40,7 +40,7 @@ public class Animal {
         this.age = age;
     }
     public void setAgeZero(int age) {
-        this.age = 10000;
+        this.age = 20;
     }
     @Override
     public String toString() {

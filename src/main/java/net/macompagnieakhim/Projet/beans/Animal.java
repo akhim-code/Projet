7c5bafc4f734b30e3b8e70514f1935af36fd622d
ame17 <antoine.meurville17@gmail.com>
2022-03-25 12:36:27 +0100

    commit (antoine) setAgeZero = 3

--- src/main/java/net/macompagnieakhim/Projet/beans/Animal.java
+++ src/main/java/net/macompagnieakhim/Projet/beans/Animal.java
@@ -36,7 +36,7 @@ public class Animal {
         this.age = age;
     }
     public void setAgeZero(int age) {
-        this.age = 1;
+        this.age = 3;
     }
     @Override
     public String toString() {
